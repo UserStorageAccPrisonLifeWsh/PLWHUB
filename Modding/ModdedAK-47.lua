448adae6afe7c14de28e85fab4b1c348ce8663e9
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2024-12-30 00:22:38 +0100

    Update and rename ModdedAK-47 to ModdedAK-47.lua

--- Modding/ModdedAK-47.lua
+++ Modding/ModdedAK-47.lua
@@ -1,9 +1,9 @@
-local weapon = require(game.Players.LocalPlayer.Backpack["Remington 870"].GunStates)
+local weapon = require(game.Players.LocalPlayer.Backpack["Ak-47"].GunStates)
 
 weapon["Damage"] = 20
 weapon["FireRate"] = 0.01
 weapon["AutoFire"] = true
-weapon["Spread"] = 5
+weapon["Spread"] = 1
 weapon["Range"] = math.huge
 weapon["ReloadTime"] = 0
 weapon["Bullets"] = 999999
