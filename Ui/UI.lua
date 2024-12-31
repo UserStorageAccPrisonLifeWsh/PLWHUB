0bd01073b071cf1691c8bb46d01ac89a0326851e
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2024-12-31 03:56:30 +0100

    Update UI.lua

--- Ui/UI.lua
+++ Ui/UI.lua
@@ -1,10 +1,5 @@
 -- Instand Stuff:
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Ui/PlayersUi.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/WalkAndJump.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/ToolBar.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/INFHealth.lua"))()
-
 -- UI & Scripts:
 
 
