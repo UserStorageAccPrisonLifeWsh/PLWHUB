a8dbf0dd63d5666423edb4555b73890ba24714f5
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2024-12-31 04:00:19 +0100

    Update MainSource.lua

--- Ui/MainSource.lua
+++ Ui/MainSource.lua
@@ -2,3 +2,8 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonL
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/WalkAndJump.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/ToolBar.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/INFHealth.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Ui/UI.lua"))()
+
+Print("| Weshky Hub Loaded! |")
+
+-- loadstring(game:HttpGet(""))() -- Discord Webhook!
