7b9ce6ffe00310dcd51273fe309a536cd88cf868
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2024-12-31 03:39:40 +0100

    Update UI.lua

--- Ui/UI.lua
+++ Ui/UI.lua
@@ -1,9 +1,10 @@
 -- Instand Stuff:
 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/INFHealth.lua')))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/ToolBar.lua')))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/WalkAndJump.lua')))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Ui/PlayersUi.lua')))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/INFHealth.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/ToolBar.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/WalkAndJump.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Ui/PlayersUi.lua"))()
 
 -- UI & Scripts:
 
