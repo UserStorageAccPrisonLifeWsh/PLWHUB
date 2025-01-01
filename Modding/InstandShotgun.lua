d97a400aeff4ca81bddc39d1018ab3c81134a994
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-01 22:20:53 +0100

    Update and rename InstandShotgun.lua to Yard.lua

--- Modding/InstandShotgun.lua
+++
@@ -1,9 +0,0 @@
-local weapon = require(game.Players.LocalPlayer.Backpack["Remington 870"].GunStates)
-
-weapon["Damage"] = 999
-weapon["FireRate"] = 0.1
-weapon["AutoFire"] = true
-weapon["Spread"] = 50
-weapon["Range"] = math.huge
-weapon["ReloadTime"] = 0
-weapon["Bullets"] = 100
