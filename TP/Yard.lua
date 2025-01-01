fe9f6c9116543c2b13f83e7f0447e16436307764
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-01 22:34:03 +0100

    Update Yard.lua

--- TP/Yard.lua
+++ TP/Yard.lua
@@ -3,8 +3,4 @@ local character = player.Character or player.CharacterAdded:Wait()
 local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
 local targetPosition = CFrame.new(787.005005, 97.999939, 2464.3584, 0.999848306, 2.02913615e-08, -0.0174189601, -2.06066915e-08, 1, -1.79231563e-08, 0.0174189601, 1.82793833e-08, 0.999848306)
-
-
-local originalPosition = humanoidRootPart.CFrame
-
 humanoidRootPart.CFrame = targetPosition
