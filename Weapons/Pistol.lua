5759e8c059fdfb04d896396929fb8f9c5b2c3e7e
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-01 02:10:41 +0100

    Update Pistol.lua

--- Weapons/Pistol.lua
+++ Weapons/Pistol.lua
@@ -9,7 +9,7 @@ wait(0.16)
 
 workspace.Remote.ItemHandler:InvokeServer(workspace["Prison_ITEMS"].giver["M9"].ITEMPICKUP)
 
-wait(0.3)
+wait(0.2)
 
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Tp.CFrame
 workspace.Tp:Destroy()
