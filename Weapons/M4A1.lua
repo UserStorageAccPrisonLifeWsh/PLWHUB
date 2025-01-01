da5c0b550dd72998cf444070824c01aa97f0be3f
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-01 01:56:46 +0100

    Update M4A1.lua

--- Weapons/M4A1.lua
+++ Weapons/M4A1.lua
@@ -4,7 +4,7 @@ TpPart.Anchored = true;
 TpPart.Transparency = 1;
 TpPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame;
 TpPart.Name = "Tp"
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(823.720032, 100.999992, 2251.27563, 0.999391854, -7.39001322e-08, 0.0348692648, 7.57053229e-08, 1, -5.04499056e-08, -0.0348692648, 5.3059015e-08, 0.999391854)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(853.500732, 103.701538, 2250.48169, 9.15527344e-05, -1, 5.24520874e-06, 5.24520874e-06, 5.24520874e-06, 1, -1, -9.15527344e-05, 5.24520874e-06)
 wait(0.16)
 
 workspace.Remote.ItemHandler:InvokeServer(workspace["Prison_ITEMS"].giver["M4A1"].ITEMPICKUP)
