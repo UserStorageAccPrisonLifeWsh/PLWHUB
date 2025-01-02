6fd9d2e02d617ff6276836fc7bcb68a0328c7bc0
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-02 18:42:04 +0100

    Update Pistol.lua

--- Weapons/Pistol.lua
+++ Weapons/Pistol.lua
@@ -5,11 +5,11 @@ TpPart.Transparency = 1;
 TpPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame;
 TpPart.Name = "Tp"
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(821.502319, 99.961853, 2253.10156, 0.996769667, 8.22011046e-08, -0.0803130567, -8.17195485e-08, 1, 9.28285271e-09, 0.0803130567, -2.68971934e-09, 0.996769667)
-wait(0.16)
+wait(0.2)
 
 workspace.Remote.ItemHandler:InvokeServer(workspace["Prison_ITEMS"].giver["M9"].ITEMPICKUP)
 
-wait(0.2)
+wait(0.25)
 
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Tp.CFrame
 workspace.Tp:Destroy()
