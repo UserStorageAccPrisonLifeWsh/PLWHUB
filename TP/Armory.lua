09c939446a96c3476243586b79119a01d40d7f64
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-02 18:55:35 +0100

    Update Armory.lua

--- TP/Armory.lua
+++ TP/Armory.lua
@@ -1,10 +1,6 @@
-local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+local TpPart = Instance.new("Part", workspace)
 
-local targetPosition = CFrame.new(837.172058, 99.9899979, 2267.62744, 0.999993086, 3.25096372e-09, 0.00371100102, -3.43431616e-09, 1, 4.94014571e-08, -0.00371100102, -4.94138597e-08, 0.999993086)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(837.172058, 99.9899979, 2267.62744, 0.999993086, 3.25096372e-09, 0.00371100102, -3.43431616e-09, 1, 4.94014571e-08, -0.00371100102, -4.94138597e-08, 0.999993086)
+wait(0.1)
 
-
-local originalPosition = humanoidRootPart.CFrame
-
-humanoidRootPart.CFrame = targetPosition
+workspace.Tp:Destroy()
