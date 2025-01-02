f945dfa885903af6f43f4179ce3d8e95fbba4f05
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-02 18:55:46 +0100

    Update Armory.lua

--- TP/Armory.lua
+++ TP/Armory.lua
@@ -1,5 +1,3 @@
-local TpPart = Instance.new("Part", workspace)
-
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(837.172058, 99.9899979, 2267.62744, 0.999993086, 3.25096372e-09, 0.00371100102, -3.43431616e-09, 1, 4.94014571e-08, -0.00371100102, -4.94138597e-08, 0.999993086)
 wait(0.1)
 
