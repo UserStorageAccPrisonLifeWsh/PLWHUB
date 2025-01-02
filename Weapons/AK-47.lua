9d23db8c155c81cb2071768e06fbd48b0d92085b
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-02 18:38:59 +0100

    Update AK-47.lua

--- Weapons/AK-47.lua
+++ Weapons/AK-47.lua
@@ -5,7 +5,7 @@ TpPart.Transparency = 1;
 TpPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame;
 TpPart.Name = "Tp"
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-943.538391, 94.1287613, 2055.99658, -0.999999642, 2.34073134e-08, -0.000844621565, 2.3361963e-08, 1, 5.3703392e-08, 0.000844621565, 5.36836424e-08, -0.999999642)
-wait(0.05)
+wait(0.1)
 
 workspace.Remote.ItemHandler:InvokeServer(workspace["Prison_ITEMS"].giver["AK-47"].ITEMPICKUP)
 
