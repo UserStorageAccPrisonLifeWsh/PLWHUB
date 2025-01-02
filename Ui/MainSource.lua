2ebd64f710b8310fb6c733d16e288e532f2e0169
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-02 23:23:47 +0100

    Delete Ui/MainSource.lua

--- Ui/MainSource.lua
+++
@@ -1,9 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Ui/PlayersUi.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/WalkAndJump.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/ToolBar.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Instand%20Script/INFHealth.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UserStorageAccPrisonLifeWsh/PLWHUB/refs/heads/main/Ui/UI.lua"))()
-
-Print("| Weshky Hub Loaded! |")
-
--- loadstring(game:HttpGet(""))() -- Discord Webhook!
