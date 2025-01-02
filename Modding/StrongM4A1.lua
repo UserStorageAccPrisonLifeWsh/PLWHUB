6451f748a8ddf1096d988fe07c3f2f7f0eb03c71
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-02 23:23:30 +0100

    Delete Modding directory

--- Modding/StrongM4A1.lua
+++
@@ -1,9 +0,0 @@
-local weapon = require(game.Players.LocalPlayer.Backpack["M4A1"].GunStates)
-
-weapon["Damage"] = 40
-weapon["FireRate"] = 0.05
-weapon["AutoFire"] = true
-weapon["Spread"] = 10
-weapon["Range"] = math.huge
-weapon["ReloadTime"] = 0
-weapon["Bullets"] = 9878678658578657857
