27ffba713c1c9f44983d79771d32698190d6dd25
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-03 21:11:59 +0100

    Create M4A1Mod.lua

---
+++ More/M4A1Mod.lua
@@ -0,0 +1,9 @@
+local weapon = require(game.Players.LocalPlayer.Backpack["M4A1"].GunStates)
+
+
+weapon["Bullets"] = 100
+weapon["Damage"] = 45
+weapon["AutoFire"] = true
+weapon["FireRate"] = 0.25
+weapon["MaxAmmo"] = 900001
+
