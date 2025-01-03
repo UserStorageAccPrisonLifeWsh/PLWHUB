d9c2e2f3415749d102e29542c7fc1f61586175d0
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-03 20:52:56 +0100

    Create Rem870Mod.lua

---
+++ More/Rem870Mod.lua
@@ -0,0 +1,10 @@
+local weapon = require(game.Players.LocalPlayer.Backpack["Remington 870"].GunStates)
+
+
+weapon["Bullets"] = 6
+weapon["Damage"] = 60
+weapon["AutoFire"] = true
+weapon["maxAmmos"] = 87
+weapon["FireRate"] = 0.25
+weapon["MaxAmmo"] = 900001
+
