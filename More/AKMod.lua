4bf9b945ce0bc4732b0e0774c51294871d48f505
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-03 21:18:43 +0100

    Create AKMod.lua

---
+++ More/AKMod.lua
@@ -0,0 +1,9 @@
+local weapon = require(game.Players.LocalPlayer.Backpack["AK-47"].GunStates)
+
+
+weapon["Bullets"] = 3
+weapon["Damage"] = 45
+weapon["AutoFire"] = true
+weapon["FireRate"] = 0.07
+weapon["MaxAmmo"] = 900001
+
