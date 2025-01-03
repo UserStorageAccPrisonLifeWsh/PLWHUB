487a851e92ed6c56841c57eed322435047c0e904
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-03 18:59:15 +0100

    Update Remington870.lua

--- Weapons/Remington870.lua
+++ Weapons/Remington870.lua
@@ -5,11 +5,11 @@ TpPart.Transparency = 1;
 TpPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame;
 TpPart.Name = "Tp"
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(823.720032, 100.999992, 2251.27563, 0.999391854, -7.39001322e-08, 0.0348692648, 7.57053229e-08, 1, -5.04499056e-08, -0.0348692648, 5.3059015e-08, 0.999391854)
-wait(0.16)
+wait(0.2)
 
 workspace.Remote.ItemHandler:InvokeServer(workspace["Prison_ITEMS"].giver["Remington 870"].ITEMPICKUP)
 
-wait(0.2)
+wait(0.25)
 
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Tp.CFrame
 workspace.Tp:Destroy()
