98970ade4ca14ea3dd164a545f1c8c9c110be2db
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-04 17:22:34 +0100

    Update AKMod.lua

--- More/AKMod.lua
+++ More/AKMod.lua
@@ -1,10 +1,14 @@
-local weapon = require(game.Players.LocalPlayer.Backpack["AK-47"].GunStates)
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait() 
+local tool = character:FindFirstChild("AK-47") 
 
+if tool and tool:FindFirstChild("GunStates") then
+    local weapon = require(tool.GunStates)
 
-weapon["Bullets"] = 3
-weapon["Damage"] = 45
-weapon["AutoFire"] = true
-weapon["FireRate"] = 0.07
-weapon["MaxAmmo"] = 900001
-weapon["CurrentAmmo"] = 900001
-
+    weapon["Bullets"] = 3
+    weapon["Damage"] = 45
+    weapon["AutoFire"] = true
+    weapon["FireRate"] = 0.07
+    weapon["MaxAmmo"] = 900001
+    weapon["CurrentAmmo"] = 900001
+end
