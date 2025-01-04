bd8c107930379520572d6e6b814fc1cdfefbcaa3
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-04 17:21:07 +0100

    Update M4A1Mod.lua

--- More/M4A1Mod.lua
+++ More/M4A1Mod.lua
@@ -1,9 +1,14 @@
-local weapon = require(game.Players.LocalPlayer.Backpack["M4A1"].GunStates)
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait() 
+local tool = character:FindFirstChild("M4A1") 
 
+if tool and tool:FindFirstChild("GunStates") then
+    local weapon = require(tool.GunStates)
 
-weapon["Bullets"] = 100
-weapon["Damage"] = 45
-weapon["AutoFire"] = true
-weapon["FireRate"] = 0.25
-weapon["MaxAmmo"] = 900001
-weapon["CurrentAmmo"] = 900001
+    weapon["Bullets"] = 100
+    weapon["Damage"] = 45
+    weapon["AutoFire"] = true
+    weapon["FireRate"] = 0.25
+    weapon["MaxAmmo"] = 900001
+    weapon["CurrentAmmo"] = 900001
+end
