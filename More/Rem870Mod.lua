7a6ffd2141f276fbaab1a085d31bd85f557347ec
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-01-04 17:20:08 +0100

    Update Rem870Mod.lua

--- More/Rem870Mod.lua
+++ More/Rem870Mod.lua
@@ -1,10 +1,15 @@
-local weapon = require(game.Players.LocalPlayer.Backpack["Remington 870"].GunStates)
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait() 
+local tool = character:FindFirstChild("Remington 870") 
 
+if tool and tool:FindFirstChild("GunStates") then
+    local weapon = require(tool.GunStates)
 
-weapon["Bullets"] = 6
-weapon["Damage"] = 60
-weapon["AutoFire"] = true
-weapon["maxAmmos"] = 87
-weapon["FireRate"] = 0.25
-weapon["MaxAmmo"] = 900001
-weapon["CurrentAmmo"] = 900001
+    weapon["Bullets"] = 6
+    weapon["Damage"] = 60
+    weapon["AutoFire"] = true
+    weapon["maxAmmos"] = 87
+    weapon["FireRate"] = 0.25
+    weapon["MaxAmmo"] = 900001
+    weapon["CurrentAmmo"] = 900001
+end
