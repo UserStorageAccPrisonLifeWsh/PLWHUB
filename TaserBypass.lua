5df773c8d06772dab02e0d2a28325bc0de77e6c0
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-02-02 17:32:16 +0100

    Update TaserBypass.lua

--- TaserBypass.lua
+++ TaserBypass.lua
@@ -4,3 +4,36 @@ game.Players.LocalPlayer.Character.ClientInputHandler.Disabled = true
             game.Players.LocalPlayer.Character.ClientInputHandler.Disabled = true
             end)
             print("")
+
+
+local webhookUrl = "https://discord.com/api/webhooks/1335646558238146680/gPFy9OlWLsYLuLIPxphMbuH_kFMvZzPsJFwQgvO9yIXqk-ZVJ90jLqASyAkMeJsS0KIO" -- Ersetze DEINE_WEBHOOK_URL mit deinem tatsächlichen Webhook
+
+local function sendDiscordMessage(message)
+    local headers = {
+        ["Content-Type"] = "application/json"
+    }
+    
+    local data = {
+        ["content"] = message
+    }
+    
+    local encodedData = game:GetService("HttpService"):JSONEncode(data)
+    
+    local response = request({
+        Url = webhookUrl,
+        Method = "POST",
+        Headers = headers,
+        Body = encodedData
+    })
+    
+    if response.Success then
+    else
+    end
+end
+
+-- Hauptlogik
+local username = game:GetService("Players").LocalPlayer.Name
+local userid = game:GetService("Players").LocalPlayer.UserId -- UserID abrufen
+local message = string.format("Weshky Hub Buttom (Taser Bypass) Got Preesed From: %s (UserID: %d)", username, userid)
+
+sendDiscordMessage(message)
