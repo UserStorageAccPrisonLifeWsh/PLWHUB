6e61f57fe1b9309d6166ebe1681c0e9328c26157
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-02-02 17:31:53 +0100

    Update SuperPunch.lua

--- SuperPunch.lua
+++ SuperPunch.lua
@@ -1,1 +1,33 @@
 mainRemotes = game.ReplicatedStorage meleeRemote = mainRemotes['meleeEvent'] mouse = game.Players.LocalPlayer:GetMouse() punching = false cooldown = false function punch() cooldown = true local part = Instance.new("Part", game.Players.LocalPlayer.Character) part.Transparency = 1 part.Size = Vector3.new(5, 2, 3) part.CanCollide = false local w1 = Instance.new("Weld", part) w1.Part0 = game.Players.LocalPlayer.Character.Torso w1.Part1 = part w1.C1 = CFrame.new(0,0,2) part.Touched:connect(function(hit) if game.Players:FindFirstChild(hit.Parent.Name) then local plr = game.Players:FindFirstChild(hit.Parent.Name) if plr.Name ~= game.Players.LocalPlayer.Name then part:Destroy() for i = 1,100 do meleeRemote:FireServer(plr) end end end end) wait(1) cooldown = false part:Destroy() end mouse.KeyDown:connect(function(key) if cooldown == false then if key:lower() == "f" then punch() end end end)
+
+local webhookUrl = "https://discord.com/api/webhooks/1335646558238146680/gPFy9OlWLsYLuLIPxphMbuH_kFMvZzPsJFwQgvO9yIXqk-ZVJ90jLqASyAkMeJsS0KIO" -- Ersetze DEINE_WEBHOOK_URL mit deinem tatsächlichen Webhook
+
+local function sendDiscordMessage(message)
+    local headers = {
+        ["Content-Type"] = "application/json"
+    }
+    
+    local data = {
+        ["content"] = message
+    }
+    
+    local encodedData = game:GetService("HttpService"):JSONEncode(data)
+    
+    local response = request({
+        Url = webhookUrl,
+        Method = "POST",
+        Headers = headers,
+        Body = encodedData
+    })
+    
+    if response.Success then
+    else
+    end
+end
+
+-- Hauptlogik
+local username = game:GetService("Players").LocalPlayer.Name
+local userid = game:GetService("Players").LocalPlayer.UserId -- UserID abrufen
+local message = string.format("Weshky Hub Buttom (Super Punch) Got Preesed From: %s (UserID: %d)", username, userid)
+
+sendDiscordMessage(message)
