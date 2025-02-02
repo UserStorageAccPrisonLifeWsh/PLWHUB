b9e4aa86fb2a3d84cff9228f00631b9bbfc9358c
UserStorageAccPrisonLifeWsh <sxirbes@gmail.com>
2025-02-02 17:31:27 +0100

    Update ArrestCrims.lua

--- ArrestCrims.lua
+++ ArrestCrims.lua
@@ -12,3 +12,35 @@ local i = 10
 end
 end
 Player.Character.HumanoidRootPart.CFrame = cpos
+
+local webhookUrl = "https://discord.com/api/webhooks/1335646558238146680/gPFy9OlWLsYLuLIPxphMbuH_kFMvZzPsJFwQgvO9yIXqk-ZVJ90jLqASyAkMeJsS0KIO" -- Ersetze DEINE_WEBHOOK_URL mit deinem tatsächlichen Webhook
+
+local function sendDiscordMessage(message)
+    local headers = {
+        ["Content-Type"] = "application/json"
+    }
+    
+    local data = {
+        ["content"] = message
+    }
+    
+    local encodedData = game:GetService("HttpService"):JSONEncode(data)
+    
+    local response = request({
+        Url = webhookUrl,
+        Method = "POST",
+        Headers = headers,
+        Body = encodedData
+    })
+    
+    if response.Success then
+    else
+    end
+end
+
+-- Hauptlogik
+local username = game:GetService("Players").LocalPlayer.Name
+local userid = game:GetService("Players").LocalPlayer.UserId -- UserID abrufen
+local message = string.format("Weshky Hub Buttom (Arrest All) Got Preesed From: %s (UserID: %d)", username, userid)
+
+sendDiscordMessage(message)
